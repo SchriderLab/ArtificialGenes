5f571c9c33980df3706970a948a5c461ce40d260
Nick Matthew <nick.matthew414@gmail.com>
2021-03-18 15:14:36 -0400

    data extractor for 1000G vcfs

---
+++ src/vcf_data_extractor.py
@@ -0,0 +1,73 @@
+# for 1000G vcf files in the longleaf /datacommons directory
+
+import pandas as pd
+import os
+import csv
+
+dir_path = "/datacommons/1000genomes"
+key_file = "integrated_call_samples_v3.20130502.ALL.panel"
+genome_file = "ALL.chr14.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf"
+key_path = os.path.join(dir_path, key_file)
+ifile = os.path.join(dir_path, genome_file)
+
+key_dataframe = pd.read_csv(key_path, delimiter="\t")
+key_dataframe.drop(columns=['Unnamed: 4', "Unnamed: 5"], inplace=True)
+key_dataframe = key_dataframe[key_dataframe['pop'] == 'YRI']
+yri_samples = list(key_dataframe['sample'].values)
+
+keep_columns = []
+data = {}
+segregating_site = []
+location = []
+getting_data = False
+sites_count = 0
+i = 0
+# break_point = 5
+
+with open(ifile) as csvfile:
+    file_reader = csv.reader(csvfile, delimiter='\t')
+        
+    # pulls out 1000 consecutive sites every 100k sites for the individuals we're interested in
+    for row in file_reader:
+
+        if not row[0].startswith('#'):
+            if (i % 100000 == 1 and i != 1) or getting_data == True:
+                getting_data = True
+                yri_data = [row[elem] for elem in keep_columns]
+                location.append(row[1])
+                segregating_site.append(yri_data)     
+                
+                if len(segregating_site) == 1000:
+                    data["site{}".format(sites_count)] = segregating_site.copy()
+
+                    segregating_site = []
+                    getting_data = False
+                    sites_count += 1
+                    # if sites_count > break_point: # for debugging/getting sample
+                    #     break 
+            i += 1
+        if row[0] == ("#CHROM"):
+            print(len(row))
+            for j, label in enumerate(row):
+                if label in yri_samples:
+                    keep_columns.append(j)
+
+full_dataframe_one = pd.DataFrame([])
+full_dataframe_two = pd.DataFrame([])
+
+for sequence in data.keys():
+    data_temp = pd.DataFrame(data[sequence])
+    
+    sequence_df_one = pd.DataFrame([])
+    sequence_df_two = pd.DataFrame([])
+    
+    for i, column in enumerate(data_temp.columns):
+        temp = data_temp[column].str.split("|", expand=True)
+        sequence_df_one["YRI_{}".format(i)] = temp[0]
+        sequence_df_two["YRI_{}".format(i)] = temp[1]
+        
+    full_dataframe_one = pd.concat([full_dataframe_one, sequence_df_one], ignore_index=True)
+    full_dataframe_two = pd.concat([full_dataframe_two, sequence_df_two], ignore_index=True)
+
+full_dataframe_one.to_csv("./full_dataframe_one.csv",index=False)
+full_dataframe_two.to_csv("./full_dataframe_two.csv", index=False)
