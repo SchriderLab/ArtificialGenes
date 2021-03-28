5b4289ba311d0616b4c09f7d5d69ed17954716f8
Nick Matthew <nick.matthew414@gmail.com>
2021-03-28 16:36:45 -0400

    convert files to csv data

---
+++ src/simulated_data_extractor.py
@@ -0,0 +1,45 @@
+# for simulated data
+
+import pandas as pd
+import os
+import gzip
+import argparse
+
+
+def parse_args():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--idir",
+                        default="/proj/dschridelab/simulationStudies/posSelVsBgs/backupFromPine/posSelVsBgs/sweeps/humanEquilib/trainingData/")
+    parser.add_argument("--ifile", default="None")
+    parser.add_argument("--odir", default="None")
+
+    args = parser.parse_args
+
+    if args.odir != "None" and not os.path.exists(args.odir):
+        os.mkdir(args.odir)
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    if args.ifile != "None":
+        ifiles = [os.path.join(args.idir, args.ifile)]
+    else:
+        ifiles = [os.path.join(args.idir, file) for file in os.path.listdir(args.idir) if ".gz" in file]
+
+    for file in ifiles:
+        with gzip.open(file, 'rt') as f:
+            file_content = f.read()
+            pre = file_content.split("\n")
+            data = [list(x) for x in pre if x.startswith("0") or x.startswith("1")]
+            del data[0] # first one is just file metadata
+            dataframe = pd.DataFrame(data)
+            dataframe.to_csv(os.path.join(args.odir, file.split(".")[0] + ".csv"), index=False)
+
+
+if __name__ == "__main__":
+    main()
