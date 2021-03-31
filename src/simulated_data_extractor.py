dca3e1637c4957bd5de724cdc4d6e40010afacf5
Nick Matthew <nick.matthew414@gmail.com>
2021-03-31 11:22:06 -0400

    improved

--- src/simulated_data_extractor.py
+++ src/simulated_data_extractor.py
@@ -27,12 +27,15 @@ def main():
     args = parse_args()
 
     if args.ifile != "None":
-        ifiles = [os.path.join(args.idir, args.ifile)]
+        ifiles = [args.ifile]
     else:
-        ifiles = [os.path.join(args.idir, file) for file in os.listdir(args.idir) if file.endswith(".gz")]
+        ifiles = [file for file in os.listdir(args.idir) if file.endswith(".gz")]
+
+    print(ifiles)
 
     for file in ifiles:
-        with gzip.open(file, 'rt') as f:
+        path = os.path.join(args.idir, file)
+        with gzip.open(path, 'rt') as f:
             file_content = f.read()
             pre = file_content.split("\n")
             data = [list(x) for x in pre if x.startswith("0") or x.startswith("1")]
