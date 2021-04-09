63166e73d5d26f16fc01d54477fa5f10e08cf8a1
Nick Matthew <nick.matthew414@gmail.com>
2021-04-09 11:48:28 -0400

    fixed paths

--- slurm/run_gan.sh
+++ slurm/run_gan.sh
@@ -28,9 +28,9 @@ fi
 SIMG_PATH=/proj/dschridelab/SparseNets/pytorch1.4.0-py3-cuda10.1-ubuntu16.04_production.simg
 
 if [[ "$3" != "" ]]; then
-    echo singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 ../src/train_gan.py --odir $ODIR --save_freq $SAVE_FREQ --verbose
-    singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 ../src/train_gan.py --odir $ODIR --save_freq $SAVE_FREQ --verbose
+    echo singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 src/train_gan.py --odir $ODIR --save_freq $SAVE_FREQ --verbose
+    singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 src/train_gan.py --odir $ODIR --save_freq $SAVE_FREQ --verbose
 else
-    echo singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 ../src/train_gan.py --odir $ODIR --save_freq $SAVE_FREQ --plot --verbose
-    singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 ../src/train_gan.py --odir $ODIR --save_freq $SAVE_FREQ --plot --verbose
+    echo singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 src/train_gan.py --odir $ODIR --save_freq $SAVE_FREQ --plot --verbose
+    singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 src/train_gan.py --odir $ODIR --save_freq $SAVE_FREQ --plot --verbose
 fi
--- slurm/run_wgan.sh
+++ slurm/run_wgan.sh
@@ -23,9 +23,9 @@ SAVE_FREQ=$3
 SIMG_PATH=/proj/dschridelab/SparseNets/pytorch1.4.0-py3-cuda10.1-ubuntu16.04_production.simg
 
 if [[ "$4" == "" ]]; then
-    echo singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 ../src/train_wgan-gp.py --odir $ODIR --ifile $IFILE --save_freq $SAVE_FREQ --plot --verbose
-    singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 ../src/train_wgan-gp.py --odir $ODIR --ifile $IFILE --save_freq $SAVE_FREQ --plot --verbose
+    echo singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 src/train_wgan-gp.py --odir $ODIR --ifile $IFILE --save_freq $SAVE_FREQ --plot --verbose
+    singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 src/train_wgan-gp.py --odir $ODIR --ifile $IFILE --save_freq $SAVE_FREQ --plot --verbose
 else
-    echo singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 ../src/train_wgan.py --odir $ODIR --ifile $IFILE --save_freq $SAVE_FREQ --plot --verbose
-    singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 ../src/train_wgan.py --odir $ODIR --ifile $IFILE --save_freq $SAVE_FREQ --plot --verbose
+    echo singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 src/train_wgan.py --odir $ODIR --ifile $IFILE --save_freq $SAVE_FREQ --plot --verbose
+    singularity exec --nv -B /pine -B /proj $SIMG_PATH python3 src/train_wgan.py --odir $ODIR --ifile $IFILE --save_freq $SAVE_FREQ --plot --verbose
 fi
